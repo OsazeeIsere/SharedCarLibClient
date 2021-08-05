58ea825ab0496412ac50fed82354c682b4842787
Osazee Isere <oz1.concepts@gmail.com>
2021-08-06 00:12:54 +0100

    initial commit

--- SharedCarLibClient/Program.cs
+++ SharedCarLibClient/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CarLibrary;
+
 namespace SharedCarLibClient
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Shared assembly client");
+            SportCar c = new SportCar("Pencil Light",120,90);
+            c.TurboBoost();
+                        
+            c.TurnOnRadio(true, MusicMedia.musicCd);
+            Console.WriteLine(c.ToString());
+            c.CarGift(c);
+            Console.ReadLine();
+
         }
     }
 }
